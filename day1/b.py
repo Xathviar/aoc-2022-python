f6d5a9ca66b7fb30bab895d4c90db96edc2b40c5
xathviar <afrischauf10@gmail.com>
2022-12-01 20:54:42 +0100

    Fixed dumb bug

--- day1/b.py
+++ day1/b.py
@@ -8,8 +8,8 @@ with open("input") as f:
     for line in f:
         if line.strip() == "":
             if value > highest_value:
-                s_highest_value = highest_value
                 t_highest_value = s_highest_value
+                s_highest_value = highest_value
                 highest_value = value
             elif value > s_highest_value:
                 t_highest_value = s_highest_value
@@ -19,4 +19,15 @@ with open("input") as f:
             value = 0
         else:
             value += int(line)
-print(f"{highest_value+s_highest_value+t_highest_value}")
+if value > 0:
+    if value > highest_value:
+        t_highest_value = s_highest_value
+        s_highest_value = highest_value
+        highest_value = value
+    elif value > s_highest_value:
+        t_highest_value = s_highest_value
+        s_highest_value = value
+    elif value > t_highest_value:
+        t_highest_value = value
+print(f"{highest_value} + {s_highest_value} + {t_highest_value}")
+print(f"{highest_value + s_highest_value + t_highest_value}")
