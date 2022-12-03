073c903d707b116d25e093ecd8d52858420df61e
xathviar <afrischauf10@gmail.com>
2022-12-03 12:35:32 +0100

    Finsihed both for day 3 but b.py was fairly easy

--- day3/a.py
+++ day3/a.py
@@ -1,5 +1,16 @@
 solution = 0
 with open("input") as f:
-    pass
-
+    for line in f:
+        line = line.strip()
+        compartment1 = line[0:int(len(line) / 2)]
+        compartment2 = line[int(len(line) / 2):]
+        for letter in compartment1:
+            if letter in compartment2:
+                if ord(letter) > 90:
+                    print(f"{letter}: {ord(letter) - 96}")
+                    solution += ord(letter) - 96
+                else:
+                    print(f"{letter}: {ord(letter) - 38}")
+                    solution += ord(letter) - 38
+                break
 print(solution)
--- day3/b.py
+++ day3/b.py
@@ -1,5 +1,18 @@
 solution = 0
+hlparray = []
 with open("input") as f:
-    pass
-
+    for line in f:
+        line = line.strip()
+        hlparray.append(line)
+        if len(hlparray) == 3:
+            for letter in line:
+                if letter in hlparray[0] and letter in hlparray[1]:
+                    if ord(letter) > 90:
+                        print(f"{letter}: {ord(letter) - 96}")
+                        solution += ord(letter) - 96
+                    else:
+                        print(f"{letter}: {ord(letter) - 38}")
+                        solution += ord(letter) - 38
+                    break
+            hlparray = []
 print(solution)
