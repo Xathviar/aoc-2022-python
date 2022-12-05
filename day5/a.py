23dec2266f28cc98747a682365853719c0f8f0a7
xathviar <afrischauf10@gmail.com>
2022-12-05 21:31:26 +0100

    Finsihed Day 5

--- day5/a.py
+++ day5/a.py
@@ -1,5 +1,36 @@
-solution = 0
-with open("input") as f:
-    pass
+import re
 
-print(solution)
+solution = ""
+configuration = {
+    '1': ['S', 'C', 'V', 'N'],
+    '2': ['Z', 'M', 'J', 'H', 'N', 'S'],
+    '3': ['M', 'C', 'T', 'G', 'J', 'N', 'D'],
+    '4': ['T', 'D', 'F', 'J', 'W', 'R', 'M'],
+    '5': ['P', 'F', 'H'],
+    '6': ['C', 'T', 'Z', 'H', 'J'],
+    '7': ['D', 'P', 'R', 'Q', 'F', 'S', 'L', 'Z'],
+    '8': ['C', 'S', 'L', 'H', 'D', 'F', 'P', 'W'],
+    '9': ['D', 'S', 'M', 'P', 'F', 'N', 'G', 'Z'],
+
+}
+
+
+def moveFromTo(fromLocation, toLocation, configuration):
+    configuration[toLocation].append(configuration[fromLocation][-1])
+    del configuration[fromLocation][-1]
+
+
+with open("input") as f:
+    for line in f:
+        line = line.strip()
+        arguments = line.split(" ")
+        amount = int(arguments[1])
+        fromLocation = arguments[3]
+        toLocation = arguments[5]
+        print(line)
+        for i in range(0, amount):
+            print(f"Moving from {fromLocation} to {toLocation}")
+            moveFromTo(fromLocation, toLocation, configuration)
+
+for i in configuration:
+    print(configuration[i][-1],end="")
--- day5/b.py
+++ day5/b.py
@@ -1,5 +1,37 @@
-solution = 0
-with open("input") as f:
-    pass
+import re
 
-print(solution)
+solution = ""
+configuration = {
+    '1': ['S', 'C', 'V', 'N'],
+    '2': ['Z', 'M', 'J', 'H', 'N', 'S'],
+    '3': ['M', 'C', 'T', 'G', 'J', 'N', 'D'],
+    '4': ['T', 'D', 'F', 'J', 'W', 'R', 'M'],
+    '5': ['P', 'F', 'H'],
+    '6': ['C', 'T', 'Z', 'H', 'J'],
+    '7': ['D', 'P', 'R', 'Q', 'F', 'S', 'L', 'Z'],
+    '8': ['C', 'S', 'L', 'H', 'D', 'F', 'P', 'W'],
+    '9': ['D', 'S', 'M', 'P', 'F', 'N', 'G', 'Z'],
+}
+
+with open("input") as f:
+    for line in f:
+        line = line.strip()
+        arguments = line.split(" ")
+        amount = int(arguments[1])
+        fromLocation = arguments[3]
+        toLocation = arguments[5]
+        firstPos = len(configuration[fromLocation]) - amount
+        toAppend = configuration[fromLocation][firstPos:]
+        configuration[toLocation].extend(toAppend)
+        print("###############################################")
+        print(line)
+        print(firstPos)
+        print(len(configuration[fromLocation]))
+        print(len(configuration[toLocation]))
+        print(configuration)
+        print(toAppend)
+        print("###############################################")
+        del configuration[fromLocation][firstPos:]
+
+for i in configuration:
+    print(configuration[i][-1], end="")
